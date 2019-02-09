3317999181a2e5637e66c2bd7a418db0e7ed4afd
Unknown <phirachat.k@ku.th>
2019-02-09 21:22:07 +0700

    change code eiei jubujubu

--- jQuery/myscript.js
+++ jQuery/myscript.js
@@ -1,32 +1,20 @@
 $(document).ready(function () {
     console.log('ready')
     $('#search-btn').click(function () {
-        if ($('#search-input').val() === '') {
+            console.log('searching')
+            let temp = ''
             $.ajax({
                 url: "catalog.json",
                 dataType: "json"
             }).done(function (response) {
-                console.log(response)
-                let temp = ''
                 response.map((item, index) =>
-                    temp += '<li key=' + index + '><img src=' + item.thumbnailUrl + ' height="200px" width="auto">' + item.title + '<span class="shortDes">' + item.shortDescription + '</span><span class="authors">' + item.authors + '</span><span class="publishDate">' + item.publishedDate.$date.split('T')[0] + '</span><span class="status">' + item.status + '</span></li></span>\n'
+                       ((item.title).indexOf($('#search-input').val())==-1)? null : temp += '<li key=' + index + '><img src=' + item.thumbnailUrl + '>' + item.title + '<span class="shortDes">' + item.shortDescription + '</span><span class="authors">' + item.authors + '</span><span class="publishDate">' + item.publishedDate.$date.split('T')[0] + '</span><span class="status">' + item.status + '</span></li></span>\n'
                 )
                 $('#result-search').html(temp)
             });
-        } else {
-            $.ajax({
-                url: "catalog.json",
-                dataType: "json"
-            }).done(function (response) {
-                console.log(response)
-                let temp = ''
-                response.filter((item, index) =>
-                    temp += '<li key=' + index + '><img src=' + item.thumbnailUrl + ' height="200px" width="auto">' + item.title + '<span class="shortDes">' + item.shortDescription + '</span><span class="authors">' + item.authors + '</span><span class="publishDate">' + item.publishedDate.$date.split('T')[0] + '</span><span class="status">' + item.status + '</span></li></span>\n'
-                )
-                $('#result-search').html(temp)
-            });
-        }
-
     })
-
-});
+    $('#adv-search').click(function(){
+        $('.field').hide(500);
+        $('#advance-search').show(500);
+    })
+}); 
