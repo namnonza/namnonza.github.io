abcf595fb53a7f3565f0a842c1a994af1ac28bcd
Unknown <phirachat.k@ku.th>
2019-02-09 14:23:36 +0700

    test

--- js-jquery/myscript.js
+++ js-jquery/myscript.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
             }).done(function (response) {
                 console.log(response)
                 let temp = ''
-                response.map((item, index) =>
+                response.filter((item, index) =>
                     temp += '<li key=' + index + '><img src=' + item.thumbnailUrl + ' height="200px" width="100px">' + item.title + '<span class="shortDes">' + item.shortDescription + '</span><span class="authors">' + item.authors + '</span><span class="publishDate">' + item.publishedDate.$date.split('T')[0] + '</span><span class="status">' + item.status + '</span></li></span>\n'
                 )
                 $('#result-search').html(temp)
